1366c835a25275682797d2c7c443a82db64d431c
tejangupta <tejangupta8@gmail.com>
2023-08-07 05:01:27 +0530

    folder structure added

---
+++ template.py
@@ -0,0 +1,46 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = 'diamond'
+
+list_of_files = [
+    '.github/workflows/.gitkeep',
+    f'{project_name}/__init__.py',
+    f'{project_name}/utils.py',
+    f'{project_name}/components/__init__.py',
+    f'{project_name}/config/__init__.py',
+    f'{project_name}/config/configuration.py',
+    f'{project_name}/pipeline/__init__.py',
+    f'{project_name}/entity/__init__.py',
+    f'{project_name}/entity/config_entity.py',
+    f'{project_name}/constants/__init__.py',
+    'config/config.yaml',
+    'params.yaml',
+    'schema.yaml',
+    'main.py',
+    'app.py',
+    'Dockerfile',
+    'requirements.txt',
+    'setup.py',
+    'notebooks/EDA.ipynb',
+    'static/css/.gitkeep',
+    'templates/index.html'
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != '':
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f'Creating directory:{filedir} for the file {filename}')
+
+    if not os.path.exists(filepath) or os.path.getsize(filepath) == 0:
+        with open(filepath, 'w') as f:
+            pass
+            logging.info(f'Creating empty file: {filepath}')
+    else:
+        logging.info(f'{filename} is already exists')
